a35a739de9fc13fe018cb49fd562f75e4ed008d3
Steve Ryan <1319496+VeloSteve@users.noreply.github.com>
2018-10-06 21:21:44 -0700

    New iterateOneReef function
    Doesn't do anything yet, but be sure branching works as expected.

---
+++ iterateOneReef.m
@@ -0,0 +1,12 @@
+function [outputArg1,outputArg2] = iterateOneReef(inputArg1,inputArg2)
+% iterateOneReef Try proportionality constants until a bleaching target
+% is met.
+%
+% To calibrate to the Donner database, each reef will be tested against actual
+% or statistically estimated bleaching data.  The proportionality constant which
+% best matches the data will be stored and used in later global runs.
+%
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
