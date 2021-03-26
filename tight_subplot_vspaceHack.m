5608214b49d72e49a397e6e1077b41ad7dd74664
Steve Ryan <1319496+VeloSteve@users.noreply.github.com>
2021-03-25 19:07:05 -0700

    tight subplot changes
    Some changes to tight_subplot.m by
    % Pekka Kumpulainen 21.5.2012   @tut.fi
    See license_tight_subplot.txt
    
    These are just my edits to allow rows or columns of subplots to have unequal sizes.  This is not meant to infringe on or change the original author's license.

--- tight_subplot_vspaceHack.m
+++ tight_subplot_vspaceHack.m
@@ -1,4 +1,4 @@
-function [ha, pos] = tight_subplot(Nh, Nw, gap, marg_h, marg_w, ratio_w, ratio_h)
+function [ha, pos] = tight_subplot_vspaceHack(Nh, Nw, gap, marg_h, marg_w, ratio_w, ratio_h, vGap)
 
 % tight_subplot creates "subplot" axes with adjustable gaps and margins
 %
@@ -15,6 +15,9 @@ function [ha, pos] = tight_subplot(Nh, Nw, gap, marg_h, marg_w, ratio_w, ratio_h
 %        ratio_w relative width of the axes in a single row.  For example if
 %                axes use the same units but show different ranges, this is
 %                useful.  Defaults to an array of ones.
+%        ratio_h relative height of the axes in a single column. Defaults to
+%                an array of ones.
+%        vGap    if given, overrides vertical gap with the provided list of gaps
 %
 %  out:  ha     array of handles of the axes objects
 %                   starting from upper left corner, going row-wise as in
@@ -34,11 +37,14 @@ if nargin<4 || isempty(marg_h); marg_h = .05; end
 if nargin<5 || isempty(marg_w); marg_w = .05; end
 if nargin<6 || isempty(ratio_w); ratio_w = ones([Nh, Nw]); end
 if nargin<7 || isempty(ratio_h); ratio_h = ones([Nh, 1]); end
+% if nargin<8 || isempty(vGap); newGap = gap(1) * ones([Nh-1, 1]); end
 ratioWTotal = sum(ratio_w(1, :));
-ratioHTotal = sum(ratio_h(:, 1));
+ratioHTotal = sum(ratio_h(1, :));
 
-if numel(gap)==1; 
-    gap = [gap gap];
+if isempty(vGap) && numel(gap)==1; 
+    newGap = gap * ones([Nh-1, 1]);
+else
+    newGap = vGap;
 end
 if numel(marg_w)==1; 
     marg_w = [marg_w marg_w];
@@ -47,7 +53,7 @@ if numel(marg_h)==1;
     marg_h = [marg_h marg_h];
 end
 
-axh = (1-sum(marg_h)-(Nh-1)*gap(1))/Nh; % v size of average plot, e.g. 0.0630
+axh = (1-sum(marg_h)-sum(newGap))/Nh; % v size of average plot, e.g. 0.0630
 axhUnit = Nh * axh / ratioHTotal;  % total axes height / total of ratio values  e.g. 0.0875
 
 axw = (1-sum(marg_w)-(Nw-1)*gap(2))/Nw;
@@ -68,7 +74,9 @@ for ih = 1:Nh
             'YTickLabel','');
         px = px+axwUnit*ratio_w(ix)+gap(2);
     end
-    py = py-axhUnit*ratio_h(min(ih+1, Nh))-gap(1);
+    if ih < Nh
+        py = py-axhUnit*ratio_h(min(ih+1, Nh))-newGap(ih);
+    end
 end
 if nargout > 1
     pos = get(ha,'Position');
